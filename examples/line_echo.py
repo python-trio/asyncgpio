dce92db65ac78a793a44b45a6a72fc0fee3a570c
Matthias Urlichs <matthias@urlichs.de>
2018-03-07 11:00:59 +0100

    example: show up/down event values

--- examples/line_echo.py
+++ examples/line_echo.py
@@ -17,9 +17,15 @@ async def main():
             with c.line(19).open(direction=gpio.DIRECTION_OUTPUT) as out_:
                 in_ = c.line(20)
                 n.start_soon(pling,out_)
-                with in_.monitor(gpio.REQUEST_EVENT_FALLING_EDGE):
+                with in_.monitor(gpio.REQUEST_EVENT_BOTH_EDGES):
                     async for e in in_:
-                        print(e,in_.value)
+                        if e.event_type == gpio.EVENT_RISING_EDGE:
+                            r = "up"
+                        elif e.event_type == gpio.EVENT_FALLING_EDGE:
+                            r = "down"
+                        else:
+                            raise RuntimeError("Unknown event type",e.event_type)
+                        print(r,in_.value)
 
 if __name__ == "__main__":
     trio.run(main)
